d0628b110eb6673db9abb383f0f7fdbedbd14fc2
Yusuf YILDIZ <yusfair12@gmail.com>
2021-05-18 23:59:31 +0300

    javaBootCamp

---
+++ StarbucksOdev/src/Abstract/ICustomerServices.java
@@ -0,0 +1,8 @@
+package Abstract;
+
+import Entities.Customer;
+
+public interface ICustomerServices {
+	void save(Customer customer);
+
+}
---
+++ StarbucksOdev/src/Concrete/NeroCustomerManager.java
@@ -0,0 +1,10 @@
+package Concrete;
+
+import Abstract.CustomerManager;
+import Entities.Customer;
+
+public class NeroCustomerManager extends CustomerManager {
+	public void save(Customer customer) {
+	}
+
+}
